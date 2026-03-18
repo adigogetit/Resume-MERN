361e58ed62460a5cf7095b75edf0f19908d28264
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-18 23:45:39 +0530

    Login form started

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,27 @@
 
 const Login = () => {
+    const [state, setState] = React.useState("login")
+
+    const [formData, setFormData] = React.useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData(prev => ({ ...prev, [name]: value }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+    }
+    
   return (
-    <div>
-      <h1>login</h1>
-    </div>
+    <>
+      
+    </>
   )
 }
 
