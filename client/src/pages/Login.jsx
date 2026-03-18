29e9229ca9910552ec57b19948398480507e7428
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-18 23:57:33 +0530

    color changed for login and signup form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+    <div className="flex items-center justify-center min-h-screen bg-grey-50">
       <form
         onSubmit={handleSubmit}
         className="w-full sm:w-87.5 text-center bg-white/6 border border-white/10 rounded-2xl px-8">
@@ -31,35 +31,35 @@ const Login = () => {
         <p className="text-gray-400 text-sm mt-2">Please sign in to continue</p>
 
         {state !== "login" && (
-          <div className="flex items-center mt-6 w-full bg-white/5 ring-2 ring-white/10 focus-within:ring-indigo-500/60 h-12 rounded-full overflow-hidden pl-6 gap-2 transition-all ">
+          <div className="flex items-center mt-6 w-full bg-white/5 ring-2 ring-white/10 focus-within:ring-violet-500/60 h-12 rounded-full overflow-hidden pl-6 gap-2 transition-all ">
             <User2Icon size={16} color='#6B7280'/>
             <input type="text" name="name" placeholder="Name" className="w-full bg-transparent text-white placeholder-white/60 border-none outline-none " value={formData.name} onChange={handleChange} required />
           </div>
         )}
 
-        <div className="flex items-center w-full mt-4 bg-white/5 ring-2 ring-white/10 focus-within:ring-indigo-500/60 h-12 rounded-full overflow-hidden pl-6 gap-2 transition-all ">
+        <div className="flex items-center w-full mt-4 bg-white/5 ring-2 ring-white/10 focus-within:ring-violet-500/60 h-12 rounded-full overflow-hidden pl-6 gap-2 transition-all ">
           <Mail size={13} color="#6B7280" />
           <input type="email" name="email" placeholder="Email id" className="w-full bg-transparent text-white placeholder-white/60 border-none outline-none " value={formData.email} onChange={handleChange} required />
         </div>
 
-        <div className=" flex items-center mt-4 w-full bg-white/5 ring-2 ring-white/10 focus-within:ring-indigo-500/60 h-12 rounded-full overflow-hidden pl-6 gap-2 transition-all ">
+        <div className=" flex items-center mt-4 w-full bg-white/5 ring-2 ring-white/10 focus-within:ring-violet-500/60 h-12 rounded-full overflow-hidden pl-6 gap-2 transition-all ">
           <Lock size={13} color="#6B7280"/>
           <input type="password" name="password" placeholder="Password" className="w-full bg-transparent text-white placeholder-white/60 border-none outline-none" value={formData.password} onChange={handleChange} required />
         </div>
 
         <div className="mt-4 text-left">
-          <button className="text-sm text-indigo-400 hover:underline">
+          <button className="text-sm text-violet-400 hover:underline">
             Forget password?
           </button>
         </div>
 
-        <button type="submit" className="mt-2 w-full h-11 rounded-full text-white bg-indigo-600 hover:bg-indigo-500 transition " >
+        <button type="submit" className="mt-2 w-full h-11 rounded-full text-white bg-violet-600 hover:bg-violet-500 transition " >
           {state === "login" ? "Login" : "Sign up"}
         </button>
 
         <p onClick={() => setState(prev => prev === "login" ? "register" : "login")} className="text-gray-400 text-sm mt-3 mb-11 cursor-pointer" >
           {state === "login" ? "Don't have an account?" : "Already have an account?"}
-          <span className="text-indigo-400 hover:underline ml-1">click here</span>
+          <span className="text-violet-400 hover:underline ml-1">click here</span>
         </p>
       </form>
     </div>
