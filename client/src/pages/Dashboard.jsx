9860198470d1dc0f3facdcac998b3723aa28378c
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-29 23:59:39 +0530

    started dasboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,8 @@
 
 const Dasboard = () => {
   return (
-    <div className="text-white">
-      <h1>Dasboard Pages</h1>
+    <div className="text-white max-w-7xl mx-auto px-4 py-8">
+      <p className=" text-white text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden">Welcom, Aditya</p>
     </div>
   )
 }
