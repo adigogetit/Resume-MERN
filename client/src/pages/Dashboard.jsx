11e3060fee38b1bbc5c916ba2a97f0bcfabb71fc
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-31 23:14:27 +0530

    added lable in  it

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -107,6 +107,23 @@ const Dasboard = () => {
             <h2 className='text-xl font-bold mb-4'>Upload Resume</h2>
             <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" placeholder='Enter resume title' className='w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600' required />
 
+            <div>
+              <label htmlFor="resume-input" className="block text-sm text-slate-700">
+                Select resume file
+                <div className='flex flex-col items-center justify-center gap-2 border group text-slate-400 border-slate-400 border-dashed rounded-md p-4 py-10 my-4 hover:border-green-500 hover:text-green-700 cursor-pointer transition-colors'>
+                  {resume ? (
+                    <p className='text-green-700'>{resume.name}</p>
+                  ) : (
+                    <>
+                      <UploadCloud className='size-14 stroke-1' />
+                      <p>Upload resume</p>
+                    </>
+                  )}
+                </div>
+              </label>
+
+              <input type="file" id='resume-input' accept='.pdf' hidden onChange={(e) => setResume(e.target.files[0])} />
+            </div>
 
             <button disabled={isLoading} className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors flex items-center justify-center gap-2'>
               {isLoading && <LoaderCircleIcon className='animate-spin size-4 text-white' />}
