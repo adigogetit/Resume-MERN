34b1e0c65f18a0ecbf3614821cf951da1ae4ef26
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-02 23:55:34 +0530

    Resumne builder page started

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,8 +1,24 @@
 
 const ResumeBuilder = () => {
+
+  const [resumeData, setResumeData] = useState({
+    _id: '',
+    title: '',
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false,
+  }) 
+
+
   return (
     <div>
-      <h1>resume builder</h1>
+      
     </div>
   )
 }
