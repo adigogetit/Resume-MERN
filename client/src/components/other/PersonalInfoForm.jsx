d77bf9965c69151af123a0d291789bb0fdabe74f
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-03 23:16:50 +0530

    PersonalInfoForm completed

--- client/src/components/other/PersonalInfoForm.jsx
+++ client/src/components/other/PersonalInfoForm.jsx
@@ -6,11 +6,21 @@ const PersonalInfoForm = ({ data, onChange, removeBackground, setRemoveBackgroun
         onChange({ ...data, [field]: value })
     }
 
+    const fields = [
+        { key: "full_name", label: "Full Name", icon: User, type: "text", required: true },
+        { key: "email", label: "Email Address", icon: Mail, type: "email", required: true },
+        { key: "phone", label: "Phone Number", icon: Phone, type: "tel" },
+        { key: "location", label: "Location", icon: MapPin, type: "text" },
+        { key: "profession", label: "Profession", icon: BriefcaseBusiness, type: "text" },
+        { key: "linkedin", label: "LinkedIn Profile", icon: Linkedin, type: "url" },
+        { key: "website", label: "Personal Website", icon: Globe, type: "url" }
+    ]
+
     return (
         <div>
             <h3 className='text-lg font-semibold text-gray-900'>Personal Information</h3>
             <p className='text-sm text-gray-600'>Get Started with the personal information</p>
- 
+
             <div className='flex items-center gap-2'>
                 {/* to upload img */}
                 <label>
@@ -35,7 +45,7 @@ const PersonalInfoForm = ({ data, onChange, removeBackground, setRemoveBackgroun
                         <p>Remove Background</p>
                         <label className='relative inline-flex items-center cursor-pointer text-gray-900 gap-3'>
                             <input type="checkbox" className="sr-only peer" onChange={() => setRemoveBackground(prev => !prev)} checked={removeBackground} />
-                            
+
                             <div className='w-9 h-5 bg-slate-300 rounded-full peer peer-checked:bg-violet-600 transition-colors duration-200'>
                             </div>
                             <span className='dot absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-4'></span>
@@ -43,9 +53,29 @@ const PersonalInfoForm = ({ data, onChange, removeBackground, setRemoveBackgroun
 
                     </div>
                 )}
-
-
             </div>
+
+            {fields.map((field) => {
+                const Icon = field.icon;
+                return (
+                    <div key={field.key} className='space-y-1 mt-5'>
+
+                        <label className="flex items-center gap-2 text-sm font-medium text-gray-800">
+                            <Icon className="size-4" />
+                            {field.label}
+                            {field.required && <span className="text-red-500">*</span>}
+                        </label>
+
+                        <input 
+                        type={field.type} 
+                        value={data[field.key] || ""} 
+                        onChange={(e) => handleChange(field.key, e.target.value)} 
+                        required={field.required} 
+                        className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors text-sm' 
+                        placeholder={`Enter your ${field.label.toLowerCase()}`} />
+                    </div>
+                )
+            })}
         </div>
     )
 }
