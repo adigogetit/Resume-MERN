5c663057ababffda41fcf3756e91c14658810c38
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-07 00:15:47 +0530

    completed ExperienceForm

--- client/src/components/templates/ExperienceForm.jsx
+++ client/src/components/templates/ExperienceForm.jsx
@@ -36,14 +36,15 @@ const ExperienceForm = ({ data, onChange }) => {
             <h3 className='flex items-center gap-2 text-lg font-semibold text-gray-900'> Professional Experience </h3>
             <p className='text-sm text-gray-500'>Add your job experience</p>
         </div>
-        {/* button */}
-        <button onClick={addExperience} className='flex items-center gap-2 px-3 py-1 text-sm bg-green-100 text-green-700 rounded-lg hover:bg-green-200 transition-colors'>
+        {/* add experience button */}
+        <button onClick={addExperience} className='flex items-center gap-2 px-3 py-1 text-sm bg-violet-100 text-violet-700 rounded-lg hover:bg-violet-200 transition-colors'>
             <Plus className="size-4"/>
             Add Experience
         </button>
       </div>
 
       {data.length === 0 ? (
+        // if no experience till now 
         <div className='text-center py-8 text-gray-500'>
             <Briefcase className="w-12 h-12 mx-auto mb-3 text-gray-300"/>
             <p>No work experience added yet.</p>
@@ -51,10 +52,11 @@ const ExperienceForm = ({ data, onChange }) => {
         </div>
       ): (
         <div className='space-y-4'>
-
+            {/* when will click Add Experience */}
             {data.map((experience, index)=>(
-                <div key={index} className="p-4 border border-gray-200 rounded-lg space-y-3">
+                <div key={index} className="p-4 border border-gray-300 rounded-lg space-y-3">
 
+                    {/* index and delete button */}
                     <div className='flex justify-between items-start'>
                         <h4>Experience #{index + 1}</h4>
                         <button onClick={()=> removeExperience(index)} className='text-red-500 hover:text-red-700 transition-colors'>
@@ -96,11 +98,12 @@ const ExperienceForm = ({ data, onChange }) => {
                         type="checkbox" 
                         checked={experience.is_current || false} 
                         onChange={(e)=>{updateExperience(index, "is_current", e.target.checked ? true : false); }} 
-                        className='rounded border-gray-300 text-blue-600 focus:ring-blue-500'/>
+                        className='rounded border-gray-300 text-violet-600 focus:ring-violet-500'/>
 
                         <span className='text-sm text-gray-700'>Currently working here</span>
                     </label>
 
+                    {/* testarea for job description */}
                     <div className="space-y-2">
                         <div className='flex items-center justify-between'>
 
--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -131,7 +131,7 @@ const ResumeBuilder = () => {
                   )}
               </div>
 
-              <button onClick={()=> {toast.promise(saveResume, {loading: 'Saving...'})}} className='bg-linear-to-br from-violet-100 to-violet-200 ring-violet-500 text-violet-600 ring hover:ring-violet-900 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+              <button onClick={()=> {toast.promise(saveResume, {loading: 'Saving...'})}} className='bg-linear-to-br from-violet-100 to-violet-200 hover:from-violet-200 hover:to-violet-400 text-violet-600  ring-violet-500 ring hover:ring-violet-900 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
                 Save Changes
               </button>
 
