4c865ac8a27cc562633118cb31286479ca15a72c
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-07 23:17:35 +0530

    added share visibility and download button

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -10,7 +10,6 @@ import ProfessionalSummaryForm from '../components/other/ProfessionalSummaryForm
 import ExperienceForm from '../components/other/ExperienceForm'
 import EducationForm from '../components/other/EducationForm'
 import ProjectForm from '../components/other/ProjectForm'
-import SkillsForm from '../components/other/SkillsForm'
 
 
 const ResumeBuilder = () => {
@@ -83,28 +82,28 @@ const ResumeBuilder = () => {
               <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200" />
               <hr className="absolute top-0 left-0  h-1 bg-linear-to-r from-violet-500 to-violet-600 border-none transition-all duration-2000" style={{ width: `${activeSectionIndex * 100 / (sections.length - 1)}%` }} />
 
-              
+
               {/* Section Navigation */}
               <div className="flex justify-between items-center mb-4 border-b border-gray-300 py-2">
 
                 {/* right side button */}
                 <div className='flex items-center gap-2'>
-                  <TemplateSelector selectedTemplate={resumeData.template} onChange={(template)=> setResumeData(prev => ({...prev, template}))}/>
-                  <ColorPicker selectedColor={resumeData.accent_color} onChange={(color)=>setResumeData(prev => ({...prev, accent_color: color}))}/>
+                  <TemplateSelector selectedTemplate={resumeData.template} onChange={(template) => setResumeData(prev => ({ ...prev, template }))} />
+                  <ColorPicker selectedColor={resumeData.accent_color} onChange={(color) => setResumeData(prev => ({ ...prev, accent_color: color }))} />
                 </div>
 
                 {/* left side button */}
                 <div className='flex items-center'>
                   {/* previous button */}
                   {activeSectionIndex !== 0 && (
-                    <button onClick={()=> setActiveSectionIndex((prevIndex)=> Math.max(prevIndex - 1, 0))} className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all' disabled={activeSectionIndex === 0}>
-                      <ChevronLeft className="size-4"/> Previous
+                    <button onClick={() => setActiveSectionIndex((prevIndex) => Math.max(prevIndex - 1, 0))} className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all' disabled={activeSectionIndex === 0}>
+                      <ChevronLeft className="size-4" /> Previous
                     </button>
                   )}
                   {/* next button */}
-                  <button onClick={()=> setActiveSectionIndex((prevIndex)=> Math.min(prevIndex + 1, sections.length - 1))} className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length - 1 && 'opacity-50'}`} disabled={activeSectionIndex === sections.length - 1}>
-                      Next <ChevronRight className="size-4"/>
-                    </button> 
+                  <button onClick={() => setActiveSectionIndex((prevIndex) => Math.min(prevIndex + 1, sections.length - 1))} className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length - 1 && 'opacity-50'}`} disabled={activeSectionIndex === sections.length - 1}>
+                    Next <ChevronRight className="size-4" />
+                  </button>
                 </div>
 
               </div>
@@ -112,48 +111,48 @@ const ResumeBuilder = () => {
               {/* Form Content */}
               <div className='space-y-6'>
 
-                  {activeSection.id === 'personal' && (
-                   <PersonalInfoForm 
-                   data={resumeData.personal_info} 
-                   onChange={(data)=>setResumeData(prev => ({...prev, personal_info: data }))} 
-                   removeBackground={removeBackground} 
-                   setRemoveBackground={setRemoveBackground} /> 
-                  )}
+                {activeSection.id === 'personal' && (
+                  <PersonalInfoForm
+                    data={resumeData.personal_info}
+                    onChange={(data) => setResumeData(prev => ({ ...prev, personal_info: data }))}
+                    removeBackground={removeBackground}
+                    setRemoveBackground={setRemoveBackground} />
+                )}
 
-                  {activeSection.id === 'summary' && (
-                    <ProfessionalSummaryForm
-                    data={resumeData.professional_summary} 
-                    onChange={(data)=> setResumeData(prev=> ({...prev, professional_summary: data}))} 
-                    setResumeData={setResumeData}/>
-                  )}
+                {activeSection.id === 'summary' && (
+                  <ProfessionalSummaryForm
+                    data={resumeData.professional_summary}
+                    onChange={(data) => setResumeData(prev => ({ ...prev, professional_summary: data }))}
+                    setResumeData={setResumeData} />
+                )}
 
-                  {activeSection.id === 'experience' && (
-                    <ExperienceForm
-                    data={resumeData.experience} 
-                    onChange={(data)=> setResumeData(prev=> ({...prev, experience: data}))}/>
-                  )}
+                {activeSection.id === 'experience' && (
+                  <ExperienceForm
+                    data={resumeData.experience}
+                    onChange={(data) => setResumeData(prev => ({ ...prev, experience: data }))} />
+                )}
 
-                  {activeSection.id == 'education' && (
-                    <EducationForm
+                {activeSection.id == 'education' && (
+                  <EducationForm
                     data={resumeData.education}
-                    onChange={(data)=> setResumeData(prev=> ({...prev, education: data}))}/>
-                  )}
+                    onChange={(data) => setResumeData(prev => ({ ...prev, education: data }))} />
+                )}
 
-                  {activeSection.id === 'projects' && (
-                    <ProjectForm
-                    data={resumeData.project} 
-                    onChange={(data)=> setResumeData(prev=> ({...prev, project: data}))}/>
-                  )}
+                {activeSection.id === 'projects' && (
+                  <ProjectForm
+                    data={resumeData.project}
+                    onChange={(data) => setResumeData(prev => ({ ...prev, project: data }))} />
+                )}
 
-                  {activeSection.id === 'skills' && (
-                    <SkillsForm 
-                    data={resumeData.skills} 
-                    onChange={(data)=> setResumeData(prev=> ({...prev, skills: data}))}/>
-                  )}
+                {activeSection.id === 'skills' && (
+                  <SkillsForm
+                    data={resumeData.skills}
+                    onChange={(data) => setResumeData(prev => ({ ...prev, skills: data }))} />
+                )}
 
               </div>
 
-              <button onClick={()=> {toast.promise(saveResume, {loading: 'Saving...'})}} className='bg-linear-to-br from-violet-100 to-violet-200 hover:from-violet-200 hover:to-violet-400 text-violet-600  ring-violet-500 ring hover:ring-violet-900 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+              <button onClick={() => { toast.promise(saveResume, { loading: 'Saving...' }) }} className='bg-linear-to-br from-violet-100 to-violet-200 hover:from-violet-200 hover:to-violet-400 text-violet-600  ring-violet-500 ring hover:ring-violet-900 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
                 Save Changes
               </button>
 
@@ -161,15 +160,39 @@ const ResumeBuilder = () => {
           </div>
 
           {/* Right pannel preview */}
-            <div className='lg:col-span-7 max-lg:mt-6'>
-              <div className='relative w-full'>
+          <div className='lg:col-span-7 max-lg:mt-6'>
 
-                <div className='absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2'>
-                </div>
+            {/* imp buttons */}
+            <div className='relative w-full'>
+              <div className='absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2'>
 
+                {/* share */}
+                {resumeData.public && (
+                  <button 
+                  onClick={handleShare} 
+                  className='flex items-center p-2 px-4 gap-2 text-xs bg-linear-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors'>
+                    <Share2Icon className='size-4' /> Share
+                  </button>
+                )}
+
+                {/* visibility */}
+                <button 
+                onClick={changeResumeVisibility} 
+                className='flex items-center p-2 px-4 gap-2 text-xs bg-linear-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors'>
+                  {resumeData.public ? <EyeIcon className="size-4" /> : <EyeOffIcon className="size-4" />}
+                  {resumeData.public ? 'Public' : 'Private'}
+                </button>
+
+                {/*download  */}
+                <button 
+                onClick={downloadResume} 
+                className='flex items-center gap-2 px-6 py-2 text-xs bg-linear-to-br from-green-100 to-green-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors'>
+                  <DownloadIcon className='size-4' /> Download
+                </button>
               </div>
-
-              <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color}/>
+            </div>
+              {/* resume preview */}
+            <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color} />
           </div>
 
         </div>
