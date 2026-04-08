934ae98c5603b5f09bb1af9b4f040c094a0b9f7a
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-08 23:45:29 +0530

    started Preview page

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,9 +1,37 @@
+import  { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets'
+import ResumePreview from '../components/other/ResumePreview'
+import { ArrowLeftIcon } from 'lucide-react'
 
 const Preview = () => {
-  return (
-    <div>
-      <h1>Preview</h1>
+  const { resumeId } = useParams()
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [resumeData, setResumeData] = useState(null)
+
+  const loadResume = async () => {
+    setResumeData(dummyResumeData.find(resume => resume._id === resumeId || null))
+  }
+
+  useEffect(()=>{
+    loadResume()
+  },[])
+
+  return resumeData ? (
+    // after loading it will show
+    <div className='bg-slate-100'>
+      <div className='max-w-3xl mx-auto py-10'>
+        {/* resumePreview page  */}
+        <ResumePreview 
+        data={resumeData} 
+        template={resumeData.template} 
+        accentColor={resumeData.accent_color} 
+        classes='py-4 bg-white'/>
+      </div>
     </div>
+  ) : (
+
   )
 }
 
