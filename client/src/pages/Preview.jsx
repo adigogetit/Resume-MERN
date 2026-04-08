7e17b0201043d0412101ddcac64affbb21e623fa
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-08 23:59:25 +0530

    Preview page completed

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -13,6 +13,7 @@ const Preview = () => {
 
   const loadResume = async () => {
     setResumeData(dummyResumeData.find(resume => resume._id === resumeId || null))
+    setIsLoading(false);
   }
 
   useEffect(()=>{
@@ -20,9 +21,10 @@ const Preview = () => {
   },[])
 
   return resumeData ? (
-    // after loading it will show
+    // if resumne data available 
     <div className='bg-slate-100'>
       <div className='max-w-3xl mx-auto py-10'>
+
         {/* resumePreview page  */}
         <ResumePreview 
         data={resumeData} 
@@ -33,10 +35,14 @@ const Preview = () => {
     </div>
   ) : (
     <div>
-      {isLoading ? <Loader /> : (
+
+      {isLoading ? <Loader /> // when resume is opening  show loader screen
+      : (
+        // else is no resume thrn show Resume not found 
         <div className='flex flex-col items-center justify-center h-screen'>
+
           <p className='text-center text-6xl text-slate-400 font-medium'>Resume not found</p>
-          <a href="/" className='mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors'>
+          <a href="/" className='mt-6 bg-violet-500 hover:bg-violet-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-violet-400 flex items-center transition-colors'>
             <ArrowLeftIcon className='mr-2 size-4'/>
             go to home page
           </a>
