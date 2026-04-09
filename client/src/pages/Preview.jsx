c325f637b119007eb3811b56548f14e39024a9d7
Abhay-Gupta-Git <guptaabhay2618@gmail.com>
2026-04-09 13:29:15 +0530

    abhay first commit

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -41,7 +41,7 @@ const Preview = () => {
         // else is no resume thrn show Resume not found 
         <div className='flex flex-col items-center justify-center h-screen'>
 
-          <p className='text-center text-6xl text-slate-400 font-medium'>Resume not found</p>
+          <p className='text-center text-6xl text-slate-400 font-medium'> Resume not found</p>
           <a href="/" className='mt-6 bg-violet-500 hover:bg-violet-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-violet-400 flex items-center transition-colors'>
             <ArrowLeftIcon className='mr-2 size-4'/>
             go to home page
