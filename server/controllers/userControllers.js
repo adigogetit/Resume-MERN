e041d6be12202dcdd94a054784367dd301782c10
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-11 23:22:38 +0530

    controller for user registration

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,45 @@
+import User from "../models/user.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+// jwt token for authorization
+const generateToken = (userId)=>{
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '7d'})
+    return token;
+}
+
+// controller for user registration
+// POST: /api/users/register
+export const registerUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        // check if required fields are present
+        if(!name || !email || !password){
+            return res.status(400).json({message: 'Missing required fields'})
+        }
+
+        // check if user already exists
+        const user = await User.findOne({email})
+        if(user){
+            return res.status(400).json({message: 'User already exists'})
+        }
+
+        // create new user and encrypt the password
+         const hashedPassword = await bcrypt.hash(password, 10)
+
+        // to create new usser
+         const newUser = await User.create({
+            name, email, password: hashedPassword
+         })
+
+         // return success message
+         const token = generateToken(newUser._id)
+         newUser.password = undefined;
+
+         return res.status(201).json({message: 'User created successfully', token, user: newUser})
+
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
