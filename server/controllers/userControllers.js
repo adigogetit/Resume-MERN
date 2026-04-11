4e5ef6bb986f5d0d5bfd387455107e939e89c983
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-11 23:37:50 +0530

    controller for getting user by id

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -3,8 +3,8 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 // jwt token for authorization
-const generateToken = (userId)=>{
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '7d'})
+const generateToken = (userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' })
     return token;
 }
 
@@ -12,35 +12,38 @@ const generateToken = (userId)=>{
 // POST: /api/users/register
 export const registerUser = async (req, res) => {
     try {
-        const {name, email, password} = req.body;
+        const { name, email, password } = req.body;
 
         // check if required fields are present
-        if(!name || !email || !password){
-            return res.status(400).json({message: 'Missing required fields'})
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: 'Missing required fields' })
         }
 
         // check if user already exists
-        const user = await User.findOne({email})
-        if(user){
-            return res.status(400).json({message: 'User already exists'})
+        const user = await User.findOne({ email })
+        if (user) {
+            return res.status(400).json({ message: 'User already exists' })
         }
 
         // create new user and encrypt the password
-         const hashedPassword = await bcrypt.hash(password, 10)
+        const hashedPassword = await bcrypt.hash(password, 10)
 
         // to create new usser
-         const newUser = await User.create({
+        const newUser = await User.create({
             name, email, password: hashedPassword
-         })
+        })
 
-         // return success message
-         const token = generateToken(newUser._id)
-         newUser.password = undefined;
+        // generate token
+        const token = generateToken(newUser._id)
 
-         return res.status(201).json({message: 'User created successfully', token, user: newUser})
+        // Remove password before sending response (security step)
+        newUser.password = undefined;
+
+        // return success message
+        return res.status(201).json({ message: 'User created successfully', token, user: newUser })
 
     } catch (error) {
-        return res.status(400).json({message: error.message})
+        return res.status(400).json({ message: error.message })
     }
 }
 
@@ -48,26 +51,53 @@ export const registerUser = async (req, res) => {
 // POST: /api/users/login
 export const loginUser = async (req, res) => {
     try {
-        const { email, password} = req.body;
+        const { email, password } = req.body;
 
         // check if user exists
-        const user = await User.findOne({email})
-        if(!user){
-            return res.status(400).json({message: 'Invalid email or password'})
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' })
         }
 
         // check if password is correct
-        if(!user.comparePassword(password)){
-            return res.status(400).json({message: 'Invalid email or password'})
+        if (!user.comparePassword(password)) {
+            return res.status(400).json({ message: 'Invalid email or password' })
         }
 
-        // return success message
-         const token = generateToken(user._id)
-         user.password = undefined;
+        // generate token
+        const token = generateToken(user._id)
 
-         return res.status(200).json({message: 'Login successful', token, user})
+        // Remove password before sending response (security step)
+        user.password = undefined;
+
+        // return success message
+        return res.status(200).json({ message: 'Login successful', token, user })
 
     } catch (error) {
-        return res.status(400).json({message: error.message})
+        return res.status(400).json({ message: error.message })
+    }
+}
+
+
+// controller for getting user by id
+// GET: /api/users/data
+export const getUserById = async (req, res) => {
+    try {
+        const userId = req.userId;
+
+        // check if user exists
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' })
+        }
+
+        // Remove password before sending response (security step)
+        user.password = undefined;
+
+        // return user
+        return res.status(200).json({ user })
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message })
     }
 }
