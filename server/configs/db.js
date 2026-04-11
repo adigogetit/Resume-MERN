1bc1ef293bd2adcbdb27735826bbbd8026be20b5
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-11 22:56:47 +0530

    database connected

---
+++ server/configs/db.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on("connected", ()=>{
+            console.log("Database connected successfully")
+        })
+
+        let mongodbURI = process.env.MONGODB_URI;
+        const projectName = 'resume';
+
+        // to check is in env MONGODB_URI is there aur not 
+        if(!mongodbURI){
+            throw new Error("MONGODB_URI environment variable not set")
+        }
+
+        // if we have not remove / from end 
+        if(mongodbURI.endsWith('/')){
+            mongodbURI = mongodbURI.slice(0, -1)
+        }
+
+        await mongoose.connect(`${mongodbURI}/${projectName}`)
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error)
+    }
+}
+
+export default connectDB;
