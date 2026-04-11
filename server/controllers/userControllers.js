959f043b622ed59f49bee43dca1d1fb94c08a597
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-11 23:24:59 +0530

    controller for user login

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -43,3 +43,31 @@ export const registerUser = async (req, res) => {
         return res.status(400).json({message: error.message})
     }
 }
+
+// controller for user login
+// POST: /api/users/login
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password} = req.body;
+
+        // check if user exists
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message: 'Invalid email or password'})
+        }
+
+        // check if password is correct
+        if(!user.comparePassword(password)){
+            return res.status(400).json({message: 'Invalid email or password'})
+        }
+
+        // return success message
+         const token = generateToken(user._id)
+         user.password = undefined;
+
+         return res.status(200).json({message: 'Login successful', token, user})
+
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
