c830fdac254bd8080850e21398b26e65d501328b
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-14 23:12:19 +0530

    aicontroller fully completed

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -5,12 +5,14 @@ import ai from "../configs/ai.js";
 // POST: /api/ai/enhance-pro-sum
 export const enhanceProfessionalSummary = async (req, res) => {
     try {
+        // getting it from protrct middleware 
         const { userContent } = req.body;
 
         if(!userContent){
             return res.status(400).json({message: 'Missing required fields'})
         }
-
+ 
+        // giving promt to ai
        const response = await ai.chat.completions.create({
             model: process.env.OPENAI_MODEL,
             messages: [
@@ -22,7 +24,9 @@ export const enhanceProfessionalSummary = async (req, res) => {
             ],
         })
 
+        // storing response
         const enhancedContent = response.choices[0].message.content;
+        
         return res.status(200).json({enhancedContent})
 
     } catch (error) {
@@ -36,25 +40,28 @@ export const enhanceProfessionalSummary = async (req, res) => {
 export const enhanceJobDescription = async (req, res) => {
     try {
         const { userContent } = req.body;
-
+        
         if(!userContent){
             return res.status(400).json({message: 'Missing required fields'})
         }
-
-       const response = await ai.chat.completions.create({
+        
+        const response = await ai.chat.completions.create({
             model: process.env.OPENAI_MODEL,
             messages: [
                 { role: "system",
-                 content: "You are an expert in resume writing. Your task is to enhance the job description of a resume. The job description should be only in 1-2 sentence also highlighting key responsibilities and achievements. Use action verbs and quantifiable results where possible. Make it ATS-friendly. and only return text no options or anything else." },
-                {
-                    role: "user",
-                    content: userContent,
-                },
-            ],
-        })
-
+                    content: "You are an expert in resume writing. Your task is to enhance the job description of a resume. The job description should be only in 1-2 sentence also highlighting key responsibilities and achievements. Use action verbs and quantifiable results where possible. Make it ATS-friendly. and only return text no options or anything else." },
+                    {
+                        role: "user",
+                        content: userContent,
+                    },
+                ],
+            })
+            
+        // storing response
         const enhancedContent = response.choices[0].message.content;
+        
         return res.status(200).json({enhancedContent})
+        
     } catch (error) {
         return res.status(400).json({message: error.message})
     }
@@ -63,9 +70,10 @@ export const enhanceJobDescription = async (req, res) => {
 
 // controller for uploading a resume to the database
 // POST: /api/ai/upload-resume
+
 export const uploadResume = async (req, res) => {
     try {
-       
+        // upload resume text
         const {resumeText, title} = req.body;
         const userId = req.userId;
 
@@ -73,8 +81,8 @@ export const uploadResume = async (req, res) => {
             return res.status(400).json({message: 'Missing required fields'})
         }
 
+        // to get the data extracted in JSON formate with help of ai
         const systemPrompt = "You are an expert AI Agent to extract data from resume."
-
         const userPrompt = `extract data from this resume: ${resumeText}
         
         Provide data in the following JSON format with no additional text before or after:
@@ -134,12 +142,15 @@ export const uploadResume = async (req, res) => {
         response_format: {type:  'json_object'}
         })
 
+        // storing response
         const extractedData = response.choices[0].message.content;
         const parsedData = JSON.parse(extractedData)
+
+        // create new resume
         const newResume = await Resume.create({userId, title, ...parsedData})
 
         res.json({resumeId: newResume._id})
-        
+
     } catch (error) {
         return res.status(400).json({message: error.message})
     }
