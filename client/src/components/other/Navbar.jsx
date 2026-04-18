892b54f738f7033be0e3081fb3933019f90e2ebc
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-19 00:41:11 +0530

    logout done

--- client/src/components/other/Navbar.jsx
+++ client/src/components/other/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
+import { logout } from '../../app/features/authSlice'
 
 const Navbar = () => {
 
-    const user = { name: 'Aditya Singh' }
+    const {user} = useSelector(state => state.auth)
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const logoutUser = () => {
         navigate('/')
+        dispatch(logout())
     }
 
     return (
