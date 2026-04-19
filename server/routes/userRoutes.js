1b4e3c4732d4c5478429e0a17a8e48a464c85da9
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-20 01:45:09 +0530

    .

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUserById, getUserResumes, loginUser, registerUser } from "../controllers/userController.js";
-import protect from "../middlewares/authMiddleware.js";
+import protect from "../middlewares/authmiddleware.js";
 
 const userRouter = express.Router();
 
