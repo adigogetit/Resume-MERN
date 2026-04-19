a82993831b0c6be505e6f616eb49b62b855ee882
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-20 01:00:14 +0530

    completed professional summary ai feature

--- client/src/components/other/ProfessionalSummaryForm.jsx
+++ client/src/components/other/ProfessionalSummaryForm.jsx
@@ -1,7 +1,31 @@
-import { Sparkles } from "lucide-react"
-
+import { Loader2, Sparkles } from 'lucide-react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import api from '../../configs/api'
+import toast from 'react-hot-toast'
 
 const ProfessionalSummaryForm = ({data, onChange, setResumeData}) => {
+
+  const { token } = useSelector(state => state.auth)
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const generateSummary = async () => {
+    try {
+      setIsGenerating(true)
+      const prompt = `enhance my professional summary "${data}"`;
+      const response = await api.post('/api/ai/enhance-pro-sum', {userContent: prompt}, {headers: { Authorization: token }})
+
+      setResumeData(prev => ({...prev, professional_summary: response.data.enhancedContent}))
+      toast.success("Professional summary enhanced successfully!")
+      
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error.message)
+    }
+    finally{
+      setIsGenerating(false)
+    }
+  }
+
 
   return (
     <div className='space-y-4'>
@@ -14,16 +38,15 @@ const ProfessionalSummaryForm = ({data, onChange, setResumeData}) => {
         </div>
 
         {/* generate summary button using ai */}
-        <button className='flex items-center gap-2 px-3 py-2 text-sm bg-violet-200 text-violet-800  hover:bg-violet-300 transition-colors disabled:opacity-50 rounded-lg'>
-          <Sparkles className="size-4"/>
-          Ai Enhance
+        <button disabled={isGenerating} onClick={generateSummary} className='flex items-center gap-2 px-3 py-1 text-sm bg-purple-100 text-purple-700 rounded hover:bg-purple-200 transition-colors disabled:opacity-50'>
+          {isGenerating ? (<Loader2 className="size-4 animate-spin"/>) : ( <Sparkles className="size-4"/>)}
+           {isGenerating ? "Enhancing..." : "AI Enhance"}
         </button>
       </div>
 
       <div className="mt-6">
         {/* text area for writing summary */}
         <textarea value={data || ""} onChange={(e)=> onChange(e.target.value)} rows={7} className='w-full p-3 px-4 mt-2 border text-sm border-gray-500 rounded-lg focus:ring focus:ring-violet-500 focus:border-violet-500 outline-none transition-colors resize-none' placeholder='Write a compelling professional summary that highlights your key strengths and career objectives...' />
-
         <p className='text-xs text-gray-700 max-w-4/5 mx-auto text-center'>Tip: Keep it concise (3-4 sentences) and focus on your most relevant achievements and skills.</p>
       </div>
     </div>
