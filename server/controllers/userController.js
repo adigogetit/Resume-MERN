cbc65b585880e3ab3766cd4024b2c3b4abf0b0ef
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-20 01:54:34 +0530

    ...

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/user.js";
 import Resume from "../models/Resume.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
